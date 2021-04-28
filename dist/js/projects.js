f1e807a5e05f3221f6a5de619f98bfdd59c16bba
Michael S. Payne II <55241401+MSPayneII@users.noreply.github.com>
2021-04-28 11:03:48 -0400

    Add files via upload

---
+++ dist/js/projects.js
@@ -0,0 +1,149 @@
+const projects = [
+  {
+    title: "AB Status System",
+    category: "Research & Design",
+    client: "Michigan Department of State",
+    img: "./dist/assets/mdosthumb.png",
+    url: "/mdosproject.html",
+    desc:
+      "Established an automated email confirmation system for absentee ballot status updates in four Michigan cities & townships.",
+  },
+  {
+    title: "Food Route Design",
+    category: "Graphic Design",
+    client: "Adobe Design Challenge",
+    img: "./dist/assets/foodroutethumbnail.png",
+    url: "/foodroutedesignproject.html",
+    desc:
+      "A prototype intuitive community-based platform for individuals to give and receive assistance to one another in the era of COVID-19.",
+  },
+  {
+    title: "Digital Office Space",
+    category: "Web Dev",
+    client: "Chadsey Condon (CCCO)",
+    img: "./dist/assets/Chadseythumbnail.png",
+    url: "/digitalofficeproject.html",
+    desc:
+      "Created a digital office space for a Detroit-based nonprofit organization to maintain communication with its residents and distribute resources to their community.",
+  },
+  {
+    title: "Audience Expansion",
+    category: "Research & Design",
+    client: "Journeys International",
+    img: "./dist/assets/Journeysthumbnail.png",
+    url: "/journeysproject.html",
+    desc:
+      "Customer assessment on current and potential customers to provide recommendations for attracting a new audience.",
+  },
+
+  {
+    title: "Portfolio Engagement",
+    category: "Research & Design",
+    client: "UM School of Information",
+    img: "./dist/assets/umsithumbnail.png",
+    url: "/umsiproject.html",
+    desc:
+      "The project focused on gathering user feedback on a digital portfolio system and providing recommendations for its replacement based on data analysis.",
+  },
+
+  {
+    title: "Banned Books",
+    category: "Research & Design",
+    client: "American Library Association",
+    img: "./dist/assets/alathumbnail.png",
+    url: "/bannedbooksproject.html",
+    desc:
+      "The project goal was to provide an updated analysis on the usability accessibility of the Banned & Challenged Books website.",
+  },
+  {
+    title: "Software Evaluation",
+    category: "Research & Design",
+    client: "Michigan Township Association",
+    img: "./dist/assets/mtathumbnail.png",
+    url: "/mtaproject.html",
+    desc:
+      "The project goal was to provide a software assessment of virtual meeting platforms MTA membership use to host OMA-compliant meetings during the pandemic.",
+  },
+
+  {
+    title: "Social Media Dash",
+    category: "Web Dev",
+    client: "Personal Project",
+    img: "./dist/assets/socialmediadash.png",
+    url: "/socialdashproject.html",
+    desc:
+      "This project required me to build a fully responsive social media dashboard landing page with a color theme switcher.",
+  },
+  {
+    title: "EasyBank Page",
+    category: "Web Dev",
+    client: "Personal Project",
+    img: "./dist/assets/easybank.png",
+    url: "/easybankproject.html",
+    desc:
+      "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid, Flexbox, SCSS, and JavaScript for the menu.",
+  },
+  {
+    title: "Github Jobs Board",
+    category: "Web Dev",
+    client: "Personal Project",
+    img: "./dist/assets/githubjobsapi.png",
+    url: "/githubjobsproject.html",
+    desc:
+      "I built an almost fully functional jobs board using the GitHub Jobs API and React Js. This project was my first time using React!",
+  },
+  {
+    title: "Price Component",
+    category: "Web Dev",
+    client: "Personal Project",
+    img: "./dist/assets/interactcomponent.png",
+    url: "/pricecomponent.html",
+    desc:
+      "I built an interactive pricing component complete with a custom range input slider and pricing toggle in this project.",
+  },
+  {
+    title: "Card Components",
+    category: "Web Dev",
+    client: "Personal Project",
+    img: "./dist/assets/cardcomponentpng.png",
+    url: "/cardcomponentsproject.html",
+    desc:
+      "This project was allowed me to practice my layout and responsive design skills with four card components. ",
+  },
+  {
+    title: "Sign-Up Component",
+    category: "Web Dev",
+    client: "Personal Project",
+    img: "./dist/assets/signupcomponent.png",
+    url: "/signupcompproject.html",
+    desc:
+      "For this project, I built a sign-up form complete with client-side validation.",
+  },
+  {
+    title: "Food Route Prototype",
+    category: "App Dev",
+    client: "Personal Project",
+    img: "./dist/assets/foodroutedev.png",
+    url: "/foodrouteprototypeproject.html",
+    desc:
+      "This project was my first attempt at building a community-based platform for individuals to give and receive assistance to one another in the era of COVID-19.",
+  },
+  // {
+  //   title: "I-School Rankings",
+  //   category: "App Dev",
+  //   client: "React Native",
+  //   img: "./dist/assets/ischoolthumb.png",
+  //   url: "/ischoolrankproject.html",
+  //   desc:
+  //     "For this project I built an app that shows totally accurate, up to the date global rankings for iSchools all over the world JK.",
+  // },
+  {
+    title: "ListMaker 3000",
+    category: "App Dev",
+    client: "Personal Project",
+    img: "./dist/assets/listmaker3000.png",
+    url: "/listmakerproject.html",
+    desc:
+      "I built an app that allows users to create, update, and delete checklists and the items contained in those checklists using '2-level' CRUD and Firebase.",
+  },
+];
